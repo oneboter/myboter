553ebb03c45e6b9893d4d6df0c1830ea12f7dfa8
oneboter <alitabarsa001@gmail.com>
2016-03-17 21:12:09 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {218794610},--Sudo users
+    sudo_users = {154519643},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
